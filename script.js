8c8f33cac78cd1f657970dc91d32cfb83a78c49f
Edward M <edwardtmuniz@gmail.com>
2020-05-03 14:27:52 -0700

    hamburger menu working

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//add the copyright date
 document.getElementById(
   "copyright"
 ).innerHTML = `Copyright &copy; ${new Date().getFullYear()} The Rowdy Puppy`;
+
+//initialize the menu as closed
+let menuOpen = false;
+
+//onclick event for the hamburger button
+document.getElementById("hamburger").addEventListener("click", () => {
+  document.getElementById("top-menu").style.display = menuOpen
+    ? "none"
+    : "block";
+
+  //var for the ul
+  const topMenuUl = document
+    .getElementById("top-menu")
+    .querySelector("#top-menu ul");
+
+  menuOpen = !menuOpen;
+});
